6a27e1a09c5ad4e519e84c93f3426d51beef6509
Emanuele Ivaldi <emanuele.ivaldi@photobox.com>
2014-05-04 23:25:17 +0100

    mapReader now get the correct z info from the loaded map and the player is updated with that value

--- juo/src/main/java/com/foolver/juo/engine/map/MapReader.java
+++ juo/src/main/java/com/foolver/juo/engine/map/MapReader.java
@@ -12,15 +12,20 @@ public class MapReader {
   private static final Logger log = LoggerFactory.getLogger(MapReader.class);
 
   private static final String MAP0_PATH = "/map0.mul";
-  private static final int BLOCK_WIDTH = 768;
-  private static final int BLOCK_HEIGHT = 512;
-  private static final int CELLS_PER_BLOCK = 64;
   private static final int MAP_WIDTH = 6144;
   private static final int MAP_HEIGHT = 4096;
-  private static final int CELL_SIZE = Short.BYTES + Byte.BYTES;
-  private static final int BLOCK_SIZE = Integer.BYTES + CELLS_PER_BLOCK * CELL_SIZE;
-  private static final int CELL_PER_BLOCK_IN_A_COLUMN = 8;
-  private static final int BLOCK_PER_MAP_IN_A_COLUMN = 64;
+  private static final int BLOCKS_PER_MAP_IN_A_COLUMN = 8;
+  private static final int BLOCKS_PER_MAP_IN_A_ROW = 8;
+  private static final int BLOCK_WIDTH = MAP_WIDTH / BLOCKS_PER_MAP_IN_A_COLUMN;
+  private static final int BLOCK_HEIGHT = MAP_HEIGHT / BLOCKS_PER_MAP_IN_A_COLUMN;
+  private static final int CELLS_PER_BLOCK_IN_A_COLUMN = 8;
+  private static final int CELLS_PER_BLOCK_IN_A_ROW = 8;
+  private static final int CELLS_PER_BLOCK = CELLS_PER_BLOCK_IN_A_COLUMN * CELLS_PER_BLOCK_IN_A_ROW;
+  private static final int BLOCK_HEADER_SIZE = Integer.BYTES;
+  private static final int CELL_TILE_ID_SIZE = Short.BYTES;
+  private static final int CELL_LOCATION_SIZE = Byte.BYTES;
+  private static final int CELL_SIZE = CELL_TILE_ID_SIZE + CELL_LOCATION_SIZE;
+  private static final int BLOCK_SIZE = BLOCK_HEADER_SIZE + CELLS_PER_BLOCK * CELL_SIZE;
 
   private byte[] bytes = new byte[BLOCK_WIDTH * BLOCK_HEIGHT * BLOCK_SIZE];
 
@@ -37,20 +42,26 @@ public class MapReader {
   }
 
   public byte getMapCoordinateInfo(int x, int y) {
-    byte z = 0x00;
+    byte z = 0;
     if (x >= 0 && x < MAP_WIDTH && y >= 0 && y < MAP_HEIGHT) {
       int blockLockup = calculateBlockLockup(x, y);
-      int cellLockup = ((y * CELL_PER_BLOCK_IN_A_COLUMN) + x) * CELL_SIZE;
-      z = bytes[blockLockup + cellLockup + 2];
+      int cellLockup = calculateCellLokup(x, y);
+      z = bytes[blockLockup + cellLockup + CELL_TILE_ID_SIZE];
     } else {
       log.error(String.format("unable to get the altitude for coordinates %s,%s, returning 0x00", x, y));
     }
     return z;
   }
 
+  private int calculateCellLokup(int x, int y) {
+    return BLOCK_HEADER_SIZE
+        + (((y % CELLS_PER_BLOCK_IN_A_COLUMN) * CELLS_PER_BLOCK_IN_A_COLUMN) + (x % CELLS_PER_BLOCK_IN_A_COLUMN))
+        * CELL_SIZE;
+  }
+
   private int calculateBlockLockup(int x, int y) {
-    int blockX = x / CELL_PER_BLOCK_IN_A_COLUMN;
-    int blockY = y / CELL_PER_BLOCK_IN_A_COLUMN;
+    int blockX = x / BLOCKS_PER_MAP_IN_A_COLUMN;
+    int blockY = y / BLOCKS_PER_MAP_IN_A_ROW;
     return ((blockX * BLOCK_HEIGHT) + blockY) * BLOCK_SIZE;
   }
 
--- juo/src/main/java/com/foolver/juo/packetHandling/packets/processors/MoveRequestPacketProcessor.java
+++ juo/src/main/java/com/foolver/juo/packetHandling/packets/processors/MoveRequestPacketProcessor.java
@@ -3,7 +3,6 @@ package com.foolver.juo.packetHandling.packets.processors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.foolver.juo.engine.Engine;
 import com.foolver.juo.engine.map.MapReader;
 import com.foolver.juo.game.PlayerInfo;
 import com.foolver.juo.packetHandling.packets.Packet;
@@ -14,61 +13,61 @@ import com.foolver.juo.packetHandling.packets.utils.Direction;
 public class MoveRequestPacketProcessor implements PacketProcessor<MoveRequestPacket> {
 
   private static final Logger log = LoggerFactory.getLogger(MoveRequestPacketProcessor.class);
-  
+
   private MapReader mapReader;
-  
+
   public MoveRequestPacketProcessor(MapReader mapReader) {
     this.mapReader = mapReader;
   }
-  
+
   @Override
   public Packet processPacket(MoveRequestPacket packet) {
     PlayerInfo playerInfo = PlayerInfo.getInstance();
     Direction requestedDirection = packet.getDirection();
-    if(requestedDirection == playerInfo.getDir()){
+    if (requestedDirection == playerInfo.getDir()) {
       updatePlaerInfoPosition(playerInfo, requestedDirection);
-      byte z = mapReader.getMapCoordinateInfo(playerInfo.getPosX(), playerInfo.getPosY());
-      log.info(String.format("position: %s,%s,%s", playerInfo.getPosX(), playerInfo.getPosY(), z));
+      log.info(String.format("position: %s,%s,%s", playerInfo.getPosX(), playerInfo.getPosY(), playerInfo.getPosZ()));
     }
     playerInfo.setDir(requestedDirection);
     return new CharacterMoveACKPacket(packet.getSequenceNumber());
   }
 
   private void updatePlaerInfoPosition(PlayerInfo playerInfo, Direction direction) {
-    switch(direction){
+    switch (direction) {
     case NORTH:
-        playerInfo.decrementY();
-        break;
+      playerInfo.decrementY();
+      break;
     case EAST:
-        playerInfo.incrementX();
+      playerInfo.incrementX();
       break;
     case SOUTH:
-        playerInfo.incrementY();
+      playerInfo.incrementY();
       break;
     case WEST:
-        playerInfo.decrementX();
+      playerInfo.decrementX();
       break;
     case NORTH_EAST:
-        playerInfo.incrementX();
-        playerInfo.decrementY();
+      playerInfo.incrementX();
+      playerInfo.decrementY();
       break;
     case SOUTH_EAST:
-        playerInfo.incrementX();
-        playerInfo.incrementY();
+      playerInfo.incrementX();
+      playerInfo.incrementY();
       break;
     case SOUTH_WEST:
-        playerInfo.decrementX();
-        playerInfo.incrementY();
+      playerInfo.decrementX();
+      playerInfo.incrementY();
       break;
     case NORTH_WEST:
-        playerInfo.decrementX();
-        playerInfo.decrementY();
+      playerInfo.decrementX();
+      playerInfo.decrementY();
       break;
     default:
-      //TODO throw an exception here)(?)
+      // TODO throw an exception here)(?)
       log.error("wrong direction!!!");
       break;
-        
     }
+    byte posZ = mapReader.getMapCoordinateInfo(playerInfo.getPosX(), playerInfo.getPosY());
+    playerInfo.setPosZ(posZ);
   }
 }
