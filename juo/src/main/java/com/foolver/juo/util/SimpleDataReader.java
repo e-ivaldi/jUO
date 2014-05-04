8c29b5893137739263c2095dcce9022a766e096a
Emanuele Ivaldi <emanuele.ivaldi@photobox.com>
2014-05-04 16:55:05 +0100

    pickup automatically the correct char encoding based on the first byte of the buffer

--- juo/src/main/java/com/foolver/juo/util/SimpleDataReader.java
+++ juo/src/main/java/com/foolver/juo/util/SimpleDataReader.java
@@ -3,6 +3,7 @@ package com.foolver.juo.util;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class SimpleDataReader implements DataReader {
 
@@ -31,8 +32,20 @@ public class SimpleDataReader implements DataReader {
   public String readString(int length) throws IOException {
     byte[] buf = new byte[length];
     dataInputStream.readFully(buf, 0, length);
-    String s = new String(buf);
-    return s.substring(0, s.indexOf('\0'));
+    String s = new String(buf, calculateEncoding(buf[0]));
+    int endOffset = s.indexOf('\0');
+    if (endOffset != -1) {
+      s = s.substring(0, endOffset);
+    }
+    return s;
+  }
+
+  private String calculateEncoding(byte b) {
+    if (b == '\0') {
+      return StandardCharsets.UTF_16.displayName();
+    } else {
+      return StandardCharsets.UTF_8.displayName();
+    }
   }
 
   @Override
@@ -42,17 +55,17 @@ public class SimpleDataReader implements DataReader {
 
   @Override
   public void skipByte() throws IOException {
-    skip(Byte.BYTES);    
+    skip(Byte.BYTES);
   }
 
   @Override
   public void skipShort() throws IOException {
-    skip(Short.BYTES);    
+    skip(Short.BYTES);
   }
 
   @Override
   public void skipInt() throws IOException {
-    skip(Integer.BYTES);    
+    skip(Integer.BYTES);
   }
 
   @Override
