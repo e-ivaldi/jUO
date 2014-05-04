5d9b5e42c6512ccdbd5cbed04525d84cf61c3b59
Emanuele Ivaldi <emanuele.ivaldi@photobox.com>
2014-05-04 14:33:00 +0100

    added get prontable for short byte and int

--- juo/src/main/java/com/foolver/juo/util/ByteUtil.java
+++ juo/src/main/java/com/foolver/juo/util/ByteUtil.java
@@ -6,10 +6,18 @@ public class ByteUtil {
     return String.format("%02X", b);
   }
 
+  public static String getPrintable(short s) {
+    return String.format("%04X", s);
+  }
+
+  public static String getPrintable(int i) {
+    return String.format("%08X", i);
+  }
+
   public static String getPrintable(byte[] bytes) {
     StringBuffer buffer = new StringBuffer();
     for (int i = 0; i < bytes.length; i++) {
-      buffer.append(String.format("%s ",getPrintable(bytes[i])));
+      buffer.append(String.format("%s ", getPrintable(bytes[i])));
     }
     return buffer.toString();
   }
@@ -19,7 +27,7 @@ public class ByteUtil {
   }
 
   public static byte[] getPaddedBytesOfString(String s, int length) {
-    return String.format("%1$-" + length + "s", s+'\0').getBytes();
+    return String.format("%1$-" + length + "s", s + '\0').getBytes();
   }
 
 }
