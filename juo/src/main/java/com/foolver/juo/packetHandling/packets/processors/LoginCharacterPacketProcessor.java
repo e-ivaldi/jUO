cd1c22b1bfcf3446958fe8d9500deec2209977d2
e.ivaldi <emanuele.ivaldi@gmail.com>
2014-05-08 23:35:46 +0100

    fixed the bug the freezed the user after selecting an already existent character (still remain the problem when the user try to open the status when they are just logged in game)

--- juo/src/main/java/com/foolver/juo/packetHandling/packets/processors/LoginCharacterPacketProcessor.java
+++ juo/src/main/java/com/foolver/juo/packetHandling/packets/processors/LoginCharacterPacketProcessor.java
@@ -4,6 +4,7 @@ import com.foolver.juo.game.PlayerInfo;
 import com.foolver.juo.packetHandling.packets.Packet;
 import com.foolver.juo.packetHandling.packets.request.LoginCharacterPacket;
 import com.foolver.juo.packetHandling.packets.response.CharLocaleAndBodyDebugPacket;
+import com.foolver.juo.packetHandling.packets.response.CharLocaleAndBodyForLoginPlayerPacket;
 import com.foolver.juo.packetHandling.packets.response.DrawGamePlayerPacket;
 import com.foolver.juo.packetHandling.packets.response.EnableLockedClientFeaturesPacket;
 import com.foolver.juo.packetHandling.packets.response.LoginCompletePacket;
@@ -19,7 +20,6 @@ public class LoginCharacterPacketProcessor implements PacketProcessor<LoginChara
         //TODO: do I need to send a DrawGamePlayer juyst after a CharocaleAndBodyDebug ?
         new DrawGamePlayerPacket(playerInfo),
         new LoginCompletePacket(),
-        new StatusBarInfoPacket(playerInfo),
-        new EnableLockedClientFeaturesPacket());
+        new StatusBarInfoPacket(playerInfo));
   }
 }
